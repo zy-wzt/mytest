df5a29332b523d7dfa829a80b27d034549e5e146
zhangsan <zhang@itcast.cn>
2020-01-13 12:52:12 +0800

    第一次提交mytest

---
+++ demo1/src/main/java/com/itheima/test/TestDemo1.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+/**
+ * @author zy
+ * @data 2020/1/13 - 9:28
+ */
+public class TestDemo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
